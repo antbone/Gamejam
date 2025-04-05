af29c21eedbc09ad944860147705effdc17f8faf
Barry <644680768@qq.com>
2025-04-05 23:36:33 +0800

    创建Card数据脚本框架

---
+++ Assets/Scripts/Card/CardController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CardController : MonoBehaviour
+{
+    public CardData cardData;
+    public Sprite cardImage;
+    public TextMeshProUGUI reward;
+
+    private void Start()
+    {
+        
+    }
+
+    public void InitializeCardUI()
+    {
+        cardImage = cardData.cardImage;
+        reward.text = cardData.reward.ToString();
+    }
+
+    public void SelectCardFromOneOfThree()
+    {
+
+    }
+
+/*    IEnumerator SelectCardRoutine()
+    {
+
+    }*/
+
+}
